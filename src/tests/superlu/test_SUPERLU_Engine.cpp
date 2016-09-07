100f6fd0d6839a9c2c6ed798272f9b0fba34462d
DarioMangoni <dariomangoni@gmail.com>
2016-09-07 16:40:50 -0500

    add critical test for Setup + Solve phases in SuperLU

--- src/tests/superlu/test_SUPERLU_Engine.cpp
+++ src/tests/superlu/test_SUPERLU_Engine.cpp
@@ -8,7 +8,7 @@ using namespace chrono;
 
 int test_pure_vectors_input()
 {
-
+	std::cout << "Test simple solution using STL vectors" << std::endl;
 	std::vector<double> values = { 19, 12, 12, 21, 12, 12, 21, 16, 21, 5, 21, 18 };
 	std::vector<int> rowIndex = { 0,1,4,1,2,4,0,2,0,3,3,4 };
 	std::vector<int> colIndex = { 0,3,6,8,10,12 };
@@ -37,6 +37,7 @@ int test_pure_vectors_input()
 
 int test_CSR_input()
 {
+	std::cout << "Test simple solution using CSR matrix" << std::endl;
 	int rows = 5;
 	ChCSR3Matrix matCSR3(rows, rows, false);
 
@@ -81,26 +82,86 @@ int test_CSR_input()
 	m_engine.SetSolutionVector(sol.data());
 	m_engine.SetRhsVector(rhs.data());
 
-	m_engine.SuperLUCall(13);
+	m_engine.SuperLUCall(13, true);
 
 	for (auto row_sel = 0; row_sel<sol.size(); ++row_sel)
-	{
 		std::cout << sol[row_sel] << " ";
-	}
+	std::cout << std::endl;
 
+	return 0;
+}
+
+int test_SetupSolve()
+{
+	std::cout << "Test Factorization + Solution phase" << std::endl;
+	// initialize matrix
+	int rows = 5;
+	ChCSR3Matrix matCSR3(rows, rows, false);
+
+	matCSR3.SetElement(0, 0, 19);
+	matCSR3.SetElement(1, 0, 12);
+	matCSR3.SetElement(4, 0, 12);
+
+	matCSR3.SetElement(1, 1, 21);
+	matCSR3.SetElement(2, 1, 12);
+	matCSR3.SetElement(4, 1, 12);
+
+	matCSR3.SetElement(0, 2, 21);
+	matCSR3.SetElement(2, 2, 16);
+
+	matCSR3.SetElement(0, 3, 21);
+	matCSR3.SetElement(3, 3, 5);
+
+	matCSR3.SetElement(3, 4, 21);
+	matCSR3.SetElement(4, 4, 18);
+
+	matCSR3.Compress();
+
+	std::vector<double> rhs(rows, 1.0);
+	std::vector<double> sol(rows, 0.0);
+
+
+	ChSuperLUEngine m_engine;
+
+
+
+	// call for factorization
+	m_engine.SetMatrix(matCSR3);
+	m_engine.SuperLUCall(12, true);
+
+
+
+	// call for first solve
+	m_engine.SetRhsVector(rhs.data());
+	m_engine.SetSolutionVector(sol.data());
+	m_engine.SuperLUCall(33, true);
+
+	for (auto row_sel = 0; row_sel<sol.size(); ++row_sel)
+		std::cout << sol[row_sel] << " ";
+	std::cout << std::endl;
+
+	// call for second solve
+	std::fill(rhs.begin(), rhs.end(), 2.0);
+	m_engine.SuperLUCall(33, true);
+
+	for (auto row_sel = 0; row_sel<sol.size(); ++row_sel)
+		std::cout << sol[row_sel] << " ";
+	std::cout << std::endl;
 	return 0;
 }
 
 int main(int argc, char *argv[])
 {
 
-	int test2 = test_CSR_input();
+	int test3 = test_SetupSolve();
 	int test1 = test_pure_vectors_input();
+	int test2 = test_CSR_input();
+	
 
 	
 
 
 	getchar();
 	
-	return test1 || test2;
+	return test1 || test2 || test3;
 }
